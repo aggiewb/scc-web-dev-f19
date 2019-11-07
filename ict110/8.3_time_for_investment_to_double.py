bc4c998acbc2423b6f0c0f8381fa4610d0a9d257
Aggie <48076903+aggiewb@users.noreply.github.com>
2019-11-07 11:42:32 -0800

    Add exercise 8.3 and description of the program

---
+++ ict110/8.3_time_for_investment_to_double.py
@@ -0,0 +1,10 @@
+'''
+This is a program that uses a while loop to determine how long it takes
+for an investment to double at a given rate. The input will be an annualized
+interest rate, and the output is the number of years it takes an investment to
+double.
+
+Aggie Wheeler Bateman
+
+11/7/19
+'''
