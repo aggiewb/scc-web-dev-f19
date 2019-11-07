6ad6ab8b7d4cc39224a77b9ce18a3619364b2cf7
Aggie <48076903+aggiewb@users.noreply.github.com>
2019-11-07 10:46:36 -0800

    Add three functions which return class standing

--- ict110/7.4_calculate_student_class_standing.py
+++ ict110/7.4_calculate_student_class_standing.py
@@ -6,3 +6,24 @@ Aggie Wheeler Bateman
 
 11/7/19
 '''
+
+def hoursCompleted():
+    hoursCompleted = int(input("Please enter the amount of credit hours you have completed: "))
+    return hoursCompleted
+
+def determineClass():
+    hours = hoursCompleted()
+    if hours >= 26:
+        return("Senior.")
+    elif hours >=16:
+        return("Junior.")
+    elif hours >=7:
+        return("Sophomore.")
+    elif hours <7:
+        return("Freshman.")
+
+def classStandingOutput():
+    print("Hi, I'm Hoots the Owl! I can determine your class based off of the amount of credits you've earned.")
+    print("Your class standing is", determineClass())
+
+classStandingOutput()
