47cccb7094b137fa82ef78c5d91b098d24715ef2
Aggie <48076903+aggiewb@users.noreply.github.com>
2019-11-07 10:53:34 -0800

    Add file for 7.6 exercise and its description of the program

---
+++ ict110/7.6_speeding_fines.py
@@ -0,0 +1,9 @@
+'''
+This is a program that accepts a speed limit and a clocked speed and either
+prints a message that indicates the speed was illegal or prints the amount
+of the fine if the speed is illegal.
+
+Aggie Wheeler Bateman
+
+11/7/19
+'''
