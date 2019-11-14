fc48ce2b9a5f463157a076ab78e2fc43a86b9122
AggieWB <usmaggie23@gmail.com>
2019-11-14 08:56:29 -0800

    Add file for assignment 7 program and its description

---
+++ ict110/10_tip_class.py
@@ -0,0 +1,10 @@
+'''
+This program will involve creating a class called Tip that calculates the
+amount of a tip. The program will have mutators and accesors to take in the
+amount of the charge, the percent for the tip, and the tax rate. The program
+should have methods that return, the tip amount and the tax amount and the
+total cost.
+
+Aggie Wheeler Bateman
+11/14/19
+'''
