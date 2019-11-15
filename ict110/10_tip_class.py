15e1b2c87fc30e6802d43b3f840bce29a18220c4
AggieWB <usmaggie23@gmail.com>
2019-11-15 09:21:07 -0800

    Update program class with method and additional functions
    Need to adjust program to use accessor and mutator to retrieve user data

--- ict110/10_tip_class.py
+++ ict110/10_tip_class.py
@@ -11,64 +11,28 @@ Aggie Wheeler Bateman
 
 class Tip():
     def __init__(self, charge, percentTip, taxRate):
-        self.charge = float(0)
-        self.percentTip = float(0)
-        self.taxRate = float(0)
+        self.charge = charge
+        self.percentTip = (percentTip/100) * charge
+        self.taxRate = (taxRate/100) * charge
 
-    @property
-    def charge(self):
-        return self.charge
-    #this is the getter(accesor)
-
-    @charge.setter
-    def charge(self, charge):
-        self.charge = float(charge)
-    #this is the setter(mutator)
-
-    @property
-    def percentTip(self):
-        return self.percentTip
-    #this is the getter(accesor)
-
-    @percentTip.setter
-    def percentTip(self, percentTip):
-        self.percentTip = percentTip
-    #this is the setter(mutator)
-
-    def getTaxRate(self):
-        return self.taxRate
-    #this is the getter(accessor)
-
-    def setTaxRate(self, taxRate):
-        self.taxRate = taxRate
-    #this is the setter(mutator)
-
-    def tipAmount():
-        tipAmount = (percentTip/100) * charge
-        return tipAmount
-
-    def taxAmount():
-        taxAmount = (taxRate/100) * charge
-        return taxAmount
-
-    def totalCost():
-        totalCost = tipAmount() + taxAmount() + charge
-        print("The tip amount is" + tipAmount() + ".")
-        print("The tax amount is" + taxAmount() + ".")
-        print("The total cost is" + totalCost + ".")
-
+    def totalCost(self):
+        tipAmount = self.percentTip
+        taxAmount = self.taxRate
+        totalCost = self.percentTip + self.taxRate + self.charge
+        print("The tip amount is ${0:0.2f}.".format(tipAmount))
+        print("The tax amount is ${0:0.2f}.".format(taxAmount))
+        print("The total cost is ${0:0.2f}.".format(totalCost))
+                   
 def getInput():
     charge = float(input("Please enter the charge without tax and tip: "))
     percentTip = float(input("Please enter the tip percent you'd like to give: "))
     taxRate = float(input("Please enter the sales tax rate in your area: "))
     return charge, percentTip, taxRate
               
-
 def main():
     charge, percentTip, taxRate = getInput()
-    Tip.charge = charge
-    Tip.percentTip = percentTip
-    Tip.taxRate = taxRate
+    c1= Tip(charge,percentTip,taxRate)
+    c1.totalCost()
 
 main()
     
