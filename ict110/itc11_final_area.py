4c6da18ac046c08cc599a149bdf4605e82ba2bd1
AggieWB <usmaggie23@gmail.com>
2019-12-08 06:53:24 -0800

    Add program description

---
+++ ict110/itc11_final_area.py
@@ -0,0 +1,12 @@
+'''
+This is a program that contains a class called "area", that takes in two
+parameters "side1", and "side2".  The measurements are in feet. Inside the
+class there is a function that calculates the total area by multiplying side1
+and side2. There is also function that returns the total area and a __str__
+function that returns "The total area is " + [area in square feet] (put your
+variable here). In the main function, the class will be called, which prints
+the string.
+
+Aggie Wheeler Bateman
+12/8/19
+'''
